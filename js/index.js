8e9f601bd4e92e9abcdfb9b250e5ca2dc7f85c33
maggiew61 <9thflr.maggie@gmail.com>
2017-09-22 09:49:28 +0800

    up

--- js/index.js
+++ js/index.js
@@ -4,12 +4,3 @@ $(document).ready(function(){
       $("#navside1").fadeToggle();
     })
 });
-
-/*================FAQ=============*/
-// toggle +
-$(function(){
-  $("#testQuestion, #testQuestion2,#testQuestion3,#testQuestion4,#testQuestion5,#testQuestion6,#testQuestion7,#testQuestion8,#testQuestion9,#testQuestion10,#testQuestion11,#testQuestion12,#testQuestion13,#testQuestion14,#testQuestion15").click(function(){
-    $(this).find('i').toggleClass('fa fa-plus').css("color","#33ccff");
-    $(this).find('i').toggleClass('fa fa-minus').css("color","#33ccff");
-    });
-    });
